885394a911d047567c8b3401622cdbeafbd1b83b
Djiré  Aboubakar Malick <aboubakarmalick.djire@edu.esiee.fr>
2024-09-20 10:13:16 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-#### Fonction secondaire
-
-
+"cette fonction retourne si un mots ou une phrase est un palindrome"
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """definition de la fonction secondaire palindrome
+    """
+    p=p.lower()
+    table=str.maketrans('àéçèôöúùüîïêë','aeceoouuuiiee')
+    p=p.replace(' ','')
+    p=p.replace(',','')
+    p=p.replace('?','')
+    p=p.replace(';','')
+    p=p.replace('!','')
+    p=p.replace("'","")
+    p=p.replace('-','')
+    p=p.replace(':','')
+    p=p.translate(table)
+    if p==p[::-1]:
+        return True
     return False
 
 #### Fonction principale
 
 
 def main():
-
+    """
+   la fonction principale teste le programme en faisant appel à la fonction sécondaire
+   """
     # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
-        print(s, ispalindrome(s))
+for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    print(s, ispalindrome(s))
 
 
 if __name__ == "__main__":
